1305029a2a8bbedc4a7af48dc6d6537177a2caa4
Matthew VH <vanheckematt@gmail.com>
2020-10-14 12:12:48 -0500

    add views folder with home view and partials folder containing header and footer partials

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get('/', (req, res) => {
-    res.send("HOME PAGE");
+    res.render("home");
 });
 
 app.listen(3000, () => {
-    console.log("---Server is running---")
+    console.log("---Server is running---");
 })
