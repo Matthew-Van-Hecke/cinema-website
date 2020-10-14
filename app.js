094c31a4562512f09915b57e689f018e04a400dc
Matthew VH <vanheckematt@gmail.com>
2020-10-14 16:14:19 -0500

    add carousel to home view and use forEach() to generate a carousel item for each imgUrl passed in the params

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-    res.render("home");
+    const imgUrls = ["https://www.towne-cinema.com/wp-content/uploads/2020/08/tenetbanner.jpg", 
+        "https://www.towne-cinema.com/wp-content/uploads/2020/10/cocowebban.jpg", 
+        "https://www.towne-cinema.com/wp-content/uploads/2020/10/ththingwebban.jpg"];
+    res.render("home", {imgUrls});
 });
 
 app.listen(3000, () => {
